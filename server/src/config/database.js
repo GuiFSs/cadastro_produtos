b9ca8e5ffbf98ffea940ff28c0f6ac7875e36d3d
AndersonSantanna <anderson.santanna14@gmail.com>
2019-02-09 00:38:52 -0200

    error in bd

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/ecommerce', {userNewUrlParser: true})
+module.exports = mongoose.connect('mongodb://anderson.santanna:and0504@ds127825.mlab.com:27825/e-commerce', {userNewUrlParser: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio "
